1312b56e519bcb4a1e6a3fb17935d1d82e7bfb15
javadhemmati <javadhemmati192@gmail.com>
2019-03-13 16:32:46 +0330

    Project animation
    Project animation when scrolling

--- source/js/app.js
+++ source/js/app.js
@@ -1,13 +1,49 @@
 
 // JavaScript
-// window.sr = ScrollReveal();
+window.sr = ScrollReveal();
 
-// sr.reveal('h1', {
-//     delay: 0,
-//     duration: 200,
-//     origin: 'bottom',
-//     distance: '100px'
-// });
+sr.reveal('#header img', {
+    delay: 0,
+    duration: 300,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('#features div.car', {
+    delay: 200,
+    duration: 300,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('#features div.shoe', {
+    delay: 400,
+    duration: 300,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('#features div.device', {
+    delay: 600,
+    duration: 300,
+    origin: 'bottom',
+    distance: '100px'
+});
+
+sr.reveal('#discount .offer', {
+    delay: 500,
+    duration: 300,
+    origin: 'right',
+    distance: '200px'
+});
+
+sr.reveal('#discount .discountLayout img', {
+    delay: 500,
+    duration: 300,
+    origin: 'left',
+    distance: '200px'
+});
+
 
 
 
