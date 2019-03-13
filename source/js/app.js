9e6b3dc743daeaa2a6cd53f229450a9e6b170445
javadhemmati <javadhemmati192@gmail.com>
2019-03-13 16:21:12 +0330

    Completion of the project for the responsive mode
    Complete responsive Nav

--- source/js/app.js
+++ source/js/app.js
@@ -1,10 +1,25 @@
 
 // JavaScript
-window.sr = ScrollReveal(); 
+// window.sr = ScrollReveal();
 
 // sr.reveal('h1', {
 //     delay: 0,
 //     duration: 200,
 //     origin: 'bottom',
-//     distance: '100px' 
+//     distance: '100px'
 // });
+
+
+
+function showNav(){
+  var x = document.getElementById("responsive-nav");
+  if(x.className === "responsive-nav"){
+
+    x.className += " unfold";
+
+  } else {
+
+    x.className = "responsive-nav";
+
+  }
+}
